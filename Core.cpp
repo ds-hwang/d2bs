72d31bb9140fb50a48c24a8b3bc46674672ee730
okaygo <okaygo@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-01-29 02:36:36 +0000

    I fixed the issue with detecting if we can print to chat, however while doing this, I have detected that printing to chat is broken in itself. So I've kinda rewrote a bit of the commenting and added a line for the future.
    git-svn-id: https://subversion.assembla.com/svn/d2bs@42 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- Core.cpp
+++ Core.cpp
@@ -32,9 +32,9 @@ VOID Print(CHAR* szFormat, ...)
 		// the 200 character limit seems to have been lifted... longer than 200 characters didn't crash
 		D2CLIENT_PrintGameString(wOutput, 0);
 		delete[] wOutput;
-		// TODO: Fix this detection to figure out if we're in a channel or not
-	} else if(false) {
-		D2MULTI_PrintChannelText(szString, NULL);
+	} else if(findControl(4,28,410,354,298)) {
+		// TODO: Double check this function, make sure it is working as intended.
+		// D2MULTI_PrintChannelText(szString, NULL);
 	} else {
 		MessageBox(0, szString, "D2BS " D2BS_VERSION, 0);
 	}
