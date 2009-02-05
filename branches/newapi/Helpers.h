7b7a01985883ef00bad68de439c2f90006d9c769
lord2800 <lord2800@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-02-05 05:24:30 +0000

    Cleaned the whole codebase to start from scratch.
    git-svn-id: https://subversion.assembla.com/svn/d2bs@97 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- branches/newapi/Helpers.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-INT StringTokenize(CHAR * input, CHAR separator, CHAR ** tokenbuf, INT maxtokens);
-wchar_t* AnsiToUnicode(const char* str);
-char* UnicodeToAnsi(const wchar_t* str);
-bool StringToBool(const char* str);
--- branches/newapi/dde.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include <windows.h>
-
-HDDEDATA	CALLBACK DdeCallback(UINT uType, UINT uFmt, HCONV hconv, HSZ hsz1, HSZ hsz2, HDDEDATA hdata, DWORD dwData1, DWORD dwData2);
-DWORD		CreateDdeServer();
-BOOL		ShutdownDdeServer();
