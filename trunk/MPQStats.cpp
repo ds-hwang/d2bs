1e0e0255dc579c8353a9532a0a5d009209940686
TechnoHunter <TechnoHunter@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-03-25 07:10:19 +0000

    updates and fixes for getBaseStat, allows for signed/un-signed correctness
    git-svn-id: https://subversion.assembla.com/svn/d2bs@249 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/MPQStats.cpp
+++ trunk/MPQStats.cpp
@@ -5,32 +5,32 @@
 #include "debugnew/debug_new.h"
 
 MPQTables BaseStatTable[] = {
-	{0x6FDF0438, 0x6FDF0434, itemTable, ARRAYSIZE(itemTable), 0xFFFF},
-	{0xA78, 0xA80, monstatsTable, ARRAYSIZE(monstatsTable), 0xFFFF},
-	{0xB8C, 0xB94, skilldescTable, ARRAYSIZE(skilldescTable), 0xFFFF},
-	{0xB98, 0xBA0, skillsTable, ARRAYSIZE(skillsTable), 0xFFFF},
-	{0x6FDF13E4, 0x6FDF13E8, objectsTable, ARRAYSIZE(objectsTable), 0xFFFF},
-	{0xB64, 0xB6C, missilesTable, ARRAYSIZE(missilesTable), 0xFFFF},
-	{0xA90, 0xA98, monstats2Table, ARRAYSIZE(monstats2Table), 0xFFFF},
-	{0xBCC, 0xBD4, itemstatcostTable, ARRAYSIZE(itemstatcostTable), 0xFFFF},
-	{0xC58, 0xC5C, levelsTable, ARRAYSIZE(levelsTable), 0xFFFF},
-	{0xC60, 0x00, leveldefsTable, ARRAYSIZE(leveldefsTable), 0xFFFF},
-	{0x6FDF1430, 0x6FDF1434, lvlmazeTable, ARRAYSIZE(lvlmazeTable), 0xFFFF},
-	{0x6FDF1438, 0x6FDF143C, lvlsubTable, ARRAYSIZE(lvlsubTable), 0xFFFF}, // v
-	{0x6FDF1428, 0x6FDF142C, lvlwarpTable, ARRAYSIZE(lvlwarpTable), 0xFFFF}, // v
-	{0xC64, 0xC68, lvlprestTable, ARRAYSIZE(lvlprestTable), 0xFFFF},
-	{0x6FDF1418, 0x6FDF1420, lvltypesTable, ARRAYSIZE(lvltypesTable), 0xFFFF}, // v
-	{0xBC4, 0xBC8, charstatsTable, ARRAYSIZE(charstatsTable), 0xFFFF},
-	{0xC18, 0xC1C, setitemsTable, ARRAYSIZE(setitemsTable), 0xFFFF},
-	{0xC24, 0xC28, uniqueitemsTable, ARRAYSIZE(uniqueitemsTable), 0xFFFF},
-	{0xC0C, 0xC10, setsTable, ARRAYSIZE(setsTable), 0xFFFF},
-	{0xBF8, 0xBFC, itemtypesTable, ARRAYSIZE(itemtypesTable), 0xFFFF},
-	{0x6FDF0478, 0x6FDF0474, runesTable, ARRAYSIZE(runesTable), 0xFFFF}, // v
-	{0x6FDF148C, 0x6FDF1490, cubemainTable, ARRAYSIZE(cubemainTable), 0xFFFF}, //v
-	{0x6FDF0470, 0x6FDF046C, gemsTable, ARRAYSIZE(gemsTable), 0xFFFF},
-	{0x6FDF13D0, 0x0, experienceTable, ARRAYSIZE(experienceTable), 0xFFFF}, // v
-	{0xBE8, 0xBF0, pettypeTable, ARRAYSIZE(pettypeTable), 0xFFFF},
-	{0xAD4, 0xADC, superuniquesTable, ARRAYSIZE(superuniquesTable), 0xFFFF},
+	{0x6FDF0438,	0x6FDF0434,	itemTable,			ARRAYSIZE(itemTable),			0xFFFF},
+	{0xA78,			0xA80,		monstatsTable,		ARRAYSIZE(monstatsTable),		0xFFFF},
+	{0xB8C,			0xB94,		skilldescTable,		ARRAYSIZE(skilldescTable),		0xFFFF},
+	{0xB98,			0xBA0,		skillsTable,		ARRAYSIZE(skillsTable),			0xFFFF},
+	{0x6FDF13E4,	0x6FDF13E8,	objectsTable,		ARRAYSIZE(objectsTable),		0xFFFF},
+	{0xB64,			0xB6C,		missilesTable,		ARRAYSIZE(missilesTable),		0xFFFF},
+	{0xA90,			0xA98,		monstats2Table,		ARRAYSIZE(monstats2Table),		0xFFFF},
+	{0xBCC,			0xBD4,		itemstatcostTable,	ARRAYSIZE(itemstatcostTable),	0xFFFF},
+	{0xC58,			0xC5C,		levelsTable,		ARRAYSIZE(levelsTable),			0xFFFF},
+	{0xC60,			0x00,		leveldefsTable,		ARRAYSIZE(leveldefsTable),		0xFFFF},
+	{0x6FDF1430,	0x6FDF1434,	lvlmazeTable,		ARRAYSIZE(lvlmazeTable),		0xFFFF},
+	{0x6FDF1438,	0x6FDF143C,	lvlsubTable,		ARRAYSIZE(lvlsubTable),			0xFFFF}, // v
+	{0x6FDF1428,	0x6FDF142C,	lvlwarpTable,		ARRAYSIZE(lvlwarpTable),		0xFFFF}, // v
+	{0xC64,			0xC68,		lvlprestTable,		ARRAYSIZE(lvlprestTable),		0xFFFF},
+	{0x6FDF1418,	0x6FDF1420,	lvltypesTable,		ARRAYSIZE(lvltypesTable),		0xFFFF}, // v
+	{0xBC4,			0xBC8,		charstatsTable,		ARRAYSIZE(charstatsTable),		0xFFFF},
+	{0xC18,			0xC1C,		setitemsTable,		ARRAYSIZE(setitemsTable),		0xFFFF},
+	{0xC24,			0xC28,		uniqueitemsTable,	ARRAYSIZE(uniqueitemsTable),	0xFFFF},
+	{0xC0C,			0xC10,		setsTable,			ARRAYSIZE(setsTable),			0xFFFF},
+	{0xBF8,			0xBFC,		itemtypesTable,		ARRAYSIZE(itemtypesTable),		0xFFFF},
+	{0x6FDF0478,	0x6FDF0474,	runesTable,			ARRAYSIZE(runesTable),			0xFFFF}, // v
+	{0x6FDF148C,	0x6FDF1490,	cubemainTable,		ARRAYSIZE(cubemainTable),		0xFFFF}, //v
+	{0x6FDF0470,	0x6FDF046C,	gemsTable,			ARRAYSIZE(gemsTable),			0xFFFF},
+	{0x6FDF13D0,	0x0,		experienceTable,	ARRAYSIZE(experienceTable),		0xFFFF}, // v
+	{0xBE8,			0xBF0,		pettypeTable,		ARRAYSIZE(pettypeTable),		0xFFFF},
+	{0xAD4,			0xADC,		superuniquesTable,	ARRAYSIZE(superuniquesTable),	0xFFFF},
 };
 
 DWORD GetBaseTable(INT nBaseStat, INT nClassId)
@@ -94,7 +94,7 @@ DWORD FillBaseStat(JSContext* cx, jsval *argv, INT nBaseStat, INT nClassId, INT 
 		WORD wBuffer = 0;
 		BYTE bBuffer = 0;
 		CHAR* szBuffer = NULL;
-
+		
 		DWORD dwHelperSize = pTable[nStatNumber + 1].dwFieldOffset - pTable[nStatNumber].dwFieldOffset;
 
 		if(dwHelperSize > 4)
@@ -110,19 +110,16 @@ DWORD FillBaseStat(JSContext* cx, jsval *argv, INT nBaseStat, INT nClassId, INT 
 				free(szBuffer);
 				return TRUE;
 
-			case FIELDTYPE_CALC_TO_DWORD:
-				memcpy(&dwBuffer, (LPVOID)(dwRetValue+pTable[nStatNumber].dwFieldOffset), sizeof(DWORD));
-				if (dwBuffer == 0xFFFFFFFF)
-					(*argv) = INT_TO_JSVAL(-1);
-				else
-					JS_NewNumberValue(cx,(jsdouble)dwBuffer,argv);
-				return TRUE;
-
-			case FIELDTYPE_NAME_TO_DWORD:
 			case FIELDTYPE_DATA_DWORD:
-			case FIELDTYPE_DATA_DWORD_2:
 				memcpy(&dwBuffer, (LPVOID)(dwRetValue+pTable[nStatNumber].dwFieldOffset), sizeof(DWORD));
 				JS_NewNumberValue(cx,(jsdouble)dwBuffer,argv);
+				return TRUE;
+
+			case FIELDTYPE_CALC_TO_DWORD:
+			case FIELDTYPE_NAME_TO_DWORD:
+			case FIELDTYPE_DATA_DWORD_2:
+				memcpy(&dwBuffer, (LPVOID)(dwRetValue+pTable[nStatNumber].dwFieldOffset), sizeof(DWORD));
+				(*argv) = INT_TO_JSVAL(dwBuffer);
 				return TRUE;
 			
 			case FIELDTYPE_UNKNOWN_11:
@@ -130,20 +127,18 @@ DWORD FillBaseStat(JSContext* cx, jsval *argv, INT nBaseStat, INT nClassId, INT 
 				(*argv) = INT_TO_JSVAL(dwBuffer);
 				return TRUE;
 
-			case FIELDTYPE_NAME_TO_INDEX:
 			case FIELDTYPE_NAME_TO_INDEX_2:
-			case FIELDTYPE_NAME_TO_WORD:
 			case FIELDTYPE_NAME_TO_WORD_2:
-			case FIELDTYPE_CODE_TO_WORD:
 				memcpy(&wBuffer, (LPVOID)(dwRetValue+pTable[nStatNumber].dwFieldOffset), sizeof(WORD));
-				if (wBuffer == 0xFFFF)
-					(*argv) = INT_TO_JSVAL(-1);
-				else
-					(*argv) = INT_TO_JSVAL(wBuffer);
+				//JS_NewNumberValue(cx,wBuffer,argv);
+				(*argv) = INT_TO_JSVAL((short)wBuffer);
 				return TRUE;
 
+			case FIELDTYPE_NAME_TO_INDEX:
+			case FIELDTYPE_NAME_TO_WORD:
 			case FIELDTYPE_KEY_TO_WORD:
 			case FIELDTYPE_DATA_WORD:
+			case FIELDTYPE_CODE_TO_WORD:
 				memcpy(&wBuffer, (LPVOID)(dwRetValue+pTable[nStatNumber].dwFieldOffset), sizeof(WORD));
 				(*argv) = INT_TO_JSVAL(wBuffer);
 				return TRUE;
@@ -160,7 +155,7 @@ DWORD FillBaseStat(JSContext* cx, jsval *argv, INT nBaseStat, INT nClassId, INT 
 				memcpy(&dwBuffer, (LPVOID)(dwRetValue+pTable[nStatNumber].dwFieldOffset), sizeof(DWORD));
 				(*argv) = INT_TO_JSVAL((dwBuffer & (1 << pTable[nStatNumber].dwFieldLength)) ? 1 : 0);
 				return TRUE;
-			
+
 			case FIELDTYPE_ASCII_TO_CODE:
 			case FIELDTYPE_DATA_RAW:
 				szBuffer = (CHAR*)malloc(5);
