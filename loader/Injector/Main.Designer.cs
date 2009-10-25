3d616c8fc084e7ca1da4052f076f21c3f59afb39
lord2800 <lord2800@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-10-25 09:56:20 +0000

    Adding the new loader code to the repository. This is the preliminary, very hacky, release.
    git-svn-id: https://subversion.assembla.com/svn/d2bs@493 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ loader/Injector/Main.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Injector
+{
+	partial class Main
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.button1 = new System.Windows.Forms.Button();
+			this.listBox1 = new System.Windows.Forms.ListBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.label5 = new System.Windows.Forms.Label();
+			this.checkBox1 = new System.Windows.Forms.CheckBox();
+			this.button2 = new System.Windows.Forms.Button();
+			this.button3 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(12, 220);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(130, 23);
+			this.button1.TabIndex = 0;
+			this.button1.Text = "Attach";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// listBox1
+			// 
+			this.listBox1.FormattingEnabled = true;
+			this.listBox1.Location = new System.Drawing.Point(12, 25);
+			this.listBox1.Name = "listBox1";
+			this.listBox1.Size = new System.Drawing.Size(268, 160);
+			this.listBox1.TabIndex = 2;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(9, 9);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(89, 13);
+			this.label1.TabIndex = 3;
+			this.label1.Text = "Active Processes";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Location = new System.Drawing.Point(12, 188);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(94, 13);
+			this.label4.TabIndex = 8;
+			this.label4.Text = "Last Attach Status";
+			// 
+			// label5
+			// 
+			this.label5.AutoSize = true;
+			this.label5.ForeColor = System.Drawing.SystemColors.ActiveCaption;
+			this.label5.Location = new System.Drawing.Point(21, 204);
+			this.label5.Name = "label5";
+			this.label5.Size = new System.Drawing.Size(33, 13);
+			this.label5.TabIndex = 9;
+			this.label5.Text = "None";
+			// 
+			// checkBox1
+			// 
+			this.checkBox1.AutoSize = true;
+			this.checkBox1.Location = new System.Drawing.Point(204, 188);
+			this.checkBox1.Name = "checkBox1";
+			this.checkBox1.Size = new System.Drawing.Size(76, 17);
+			this.checkBox1.TabIndex = 11;
+			this.checkBox1.Text = "Auto-inject";
+			this.checkBox1.UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(159, 220);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(120, 22);
+			this.button2.TabIndex = 12;
+			this.button2.Text = "Start New Game";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(227, -1);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(52, 23);
+			this.button3.TabIndex = 13;
+			this.button3.Text = "Options";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// Main
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(296, 253);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.checkBox1);
+			this.Controls.Add(this.label5);
+			this.Controls.Add(this.label4);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.listBox1);
+			this.Controls.Add(this.button1);
+			this.MaximizeBox = false;
+			this.Name = "Main";
+			this.ShowIcon = false;
+			this.Text = "Injector";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.ListBox listBox1;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label label5;
+		private System.Windows.Forms.CheckBox checkBox1;
+		private System.Windows.Forms.Button button2;
+		private System.Windows.Forms.Button button3;
+	}
+}
+
