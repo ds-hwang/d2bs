810555d5b4d9d1a35bef7b6b26c5ab2c89abe994
lord2800 <lord2800@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2009-12-30 15:26:05 +0000

    Tagging loader-1.3
    git-svn-id: https://subversion.assembla.com/svn/d2bs@800 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ tags/loader-1.3/D2BSLoader/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace D2BSLoader
+{
+	static class Program
+	{
+		/// <summary>
+		/// The main entry point for the application.
+		/// </summary>
+		[STAThread]
+		static void Main(string[] args)
+		{
+			Application.EnableVisualStyles();
+			Application.SetCompatibleTextRenderingDefault(false);
+			Main m = new Main(args);
+			if(!m.Autoclosed)
+				Application.Run(m);
+		}
+	}
+}
