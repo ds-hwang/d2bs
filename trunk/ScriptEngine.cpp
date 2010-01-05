b05831dc640f3d713b5df302804abd690d90c623
lord2800 <lord2800@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-01-05 11:23:46 +0000

    Fixed another instacrash. Seems to be relatively stable now.
    git-svn-id: https://subversion.assembla.com/svn/d2bs@816 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/ScriptEngine.cpp
+++ trunk/ScriptEngine.cpp
@@ -309,6 +309,7 @@ JSBool branchCallback(JSContext* cx, JSScript*)
 	if(pause)
 		script->SetPauseState(false);
 
+	JS_SetContextThread(cx);
 	JS_ResumeRequest(cx, depth);
 
 	return !!!(JSBool)(script->IsAborted() || ((script->GetState() != OutOfGame) && !D2CLIENT_GetPlayerUnit()));
