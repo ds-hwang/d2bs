4782b9a4f438197dccc18a6e1b262d65ee0a6e3a
lord2800 <lord2800@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-02-24 00:03:09 +0000

    Creating the 1.13 patch branch for managing the changes to 1.13 vs. 1.12.
    git-svn-id: https://subversion.assembla.com/svn/d2bs@991 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ branches/patch-113/D2Handlers.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <windows.h>
+#include "D2Structs.h"
+
+DWORD WINAPI D2Thread(LPVOID lpParam);
+DWORD __fastcall GameInput(wchar_t* wMsg);
+LONG WINAPI GameEventHandler(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK MouseMove(int code, WPARAM wParam, LPARAM lParam);
+LRESULT CALLBACK KeyPress(int code, WPARAM wParam, LPARAM lParam);
+void GameDraw(void);
+DWORD __fastcall GamePacketReceived(BYTE* pPacket, DWORD dwSize);
+void __fastcall GameResetFrames(UnitAny* pOwner);
+void GameDrawOOG(void);
+void __fastcall WhisperHandler(char* szAcc, char* szText);
+DWORD __fastcall GameAttack(AttackStruct* pAttack);
+void __fastcall GamePlayerAssignment(UnitAny* pPlayer);
+BOOL __stdcall GameLoop(LPMSG lpMsg, HWND hWnd, UINT wMsgFIlterMin, UINT wMsgFilterMax, UINT wRemoveMsg);
+void GameLeave(void);
