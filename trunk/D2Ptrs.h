65b230cba0c6434557318356914f2e3868d8f628
eBoLAtrapO <eBoLAtrapO@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-02-26 02:06:41 +0000

    Make the pointer macros readable and a few style nits.
    git-svn-id: https://subversion.assembla.com/svn/d2bs@1004 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- trunk/D2Ptrs.h
+++ trunk/D2Ptrs.h
@@ -4,25 +4,47 @@
 
 #include "D2Structs.h"
 
-#pragma warning ( push )
-#pragma warning ( disable: 4245 )
+#pragma warning (push)
+#pragma warning (disable: 4245)
 
-#pragma optimize ( "", off )
+#pragma optimize ("", off)
 
 #ifdef _DEFINE_VARS
 
-enum {DLLNO_D2CLIENT, DLLNO_D2COMMON, DLLNO_D2GFX, DLLNO_D2LANG, DLLNO_D2WIN, DLLNO_D2NET, DLLNO_D2GAME, DLLNO_D2LAUNCH, DLLNO_FOG, DLLNO_BNCLIENT, DLLNO_STORM, DLLNO_D2CMP, DLLNO_D2MULTI};
+enum
+{
+	DLLNO_D2CLIENT,		DLLNO_D2COMMON,		DLLNO_D2GFX,	DLLNO_D2LANG,
+	DLLNO_D2WIN,		DLLNO_D2NET,		DLLNO_D2GAME,	DLLNO_D2LAUNCH,
+	DLLNO_FOG,			DLLNO_BNCLIENT,		DLLNO_STORM,	DLLNO_D2CMP,
+	DLLNO_D2MULTI
+};
 
-#define DLLOFFSET(a1,b1) ((DLLNO_##a1)|((b1)<<8))
-#define FUNCPTR(d1,v1,t1,t2,o1)	typedef t1 d1##_##v1##_t t2; d1##_##v1##_t *d1##_##v1 = (d1##_##v1##_t *)DLLOFFSET(d1,o1);
-#define VARPTR(d1,v1,t1,o1)		typedef t1 d1##_##v1##_t;    d1##_##v1##_t *p_##d1##_##v1 = (d1##_##v1##_t *)DLLOFFSET(d1,o1);
-#define ASMPTR(d1,v1,o1)			DWORD d1##_##v1 = DLLOFFSET(d1,o1);
+#define DLLOFFSET(a1,b1) \
+	((DLLNO_##a1)|((b1)<<8))
+
+#define FUNCPTR(d1,v1,t1,t2,o1) \
+	typedef t1 d1##_##v1##_t t2; \
+	d1##_##v1##_t *d1##_##v1 = (d1##_##v1##_t *)DLLOFFSET(d1,o1);
+
+#define VARPTR(d1,v1,t1,o1) \
+	typedef t1 d1##_##v1##_t; \
+	d1##_##v1##_t *p_##d1##_##v1 = (d1##_##v1##_t *)DLLOFFSET(d1,o1);
+
+#define ASMPTR(d1,v1,o1) \
+	DWORD d1##_##v1 = DLLOFFSET(d1,o1);
 
 #else
 
-#define FUNCPTR(d1,v1,t1,t2,o1)	typedef t1 d1##_##v1##_t t2; extern d1##_##v1##_t *d1##_##v1;
-#define VARPTR(d1,v1,t1,o1)		typedef t1 d1##_##v1##_t;    extern d1##_##v1##_t *p_##d1##_##v1;
-#define ASMPTR(d1,v1,o1)			extern DWORD d1##_##v1;
+#define FUNCPTR(d1,v1,t1,t2,o1) \
+	typedef t1 d1##_##v1##_t t2; \
+	extern d1##_##v1##_t *d1##_##v1;
+
+#define VARPTR(d1,v1,t1,o1) \
+	typedef t1 d1##_##v1##_t; \
+	extern d1##_##v1##_t *p_##d1##_##v1;
+
+#define ASMPTR(d1,v1,o1) \
+	extern DWORD d1##_##v1;
 
 #endif
 #define _D2PTRS_START	D2CLIENT_GetMonsterTxt
