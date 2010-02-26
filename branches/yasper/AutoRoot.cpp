ab48b7b695e66a60b3888ca6c580d4e71aef869e
lord2800 <lord2800@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-02-26 00:49:49 +0000

    This branch isn't getting merged--too complicated right now.
    git-svn-id: https://subversion.assembla.com/svn/d2bs@1001 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- branches/yasper/AutoRoot.cpp
+++
@@ -1,5 +0,0 @@
-#include "AutoRoot.h"
-#include "ScriptEngine.h"
-
-AutoRoot::AutoRoot(jsval nvar) : var(nvar) { JS_AddRoot(&var); }
-AutoRoot::~AutoRoot() { JS_RemoveRoot(&var); }
--- branches/yasper/Unit.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-UnitAny* GetUnit(CHAR* szName, DWORD dwClassId, DWORD dwType, DWORD dwMode, DWORD dwUnitId);
-UnitAny* GetNextUnit(UnitAny* pUnit, CHAR* szName, DWORD dwClassId, DWORD dwType, DWORD dwMode);
-BOOL CheckUnit(UnitAny* pUnit, CHAR* szName, DWORD dwClassId, DWORD dwType, DWORD dwMode, DWORD dwUnitId);
-INT GetUnitHP(UnitAny* pUnit);
-INT GetUnitMP(UnitAny* pUnit);
