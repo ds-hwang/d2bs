5151a8d3e4d37feece3e74c4a5fce0c8424f985d
lord2800 <lord2800@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-03-30 01:27:00 +0000

    Remaking this branch because this copy is stale.
    git-svn-id: https://subversion.assembla.com/svn/d2bs@1037 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- branches/patch-113/Game.h
+++
@@ -1,4 +0,0 @@
-#include <windows.h>
-#include "D2Ptrs.h"
-
-void SendGold(int nGold, int nMode);
--- branches/patch-113/stringhash.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <stddef.h>
-
-unsigned __int32 __fastcall sfh (const char * data, int len);
