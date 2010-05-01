2fd16f8b75648779f9a64a7a99fafe26d25d8e16
lord2800 <lord2800@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2010-05-01 19:59:39 +0000

    Fixed CloseNPCInteract function pointer.
    git-svn-id: https://subversion.assembla.com/svn/d2bs@1080 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- branches/patch-113/D2Ptrs.h
+++ branches/patch-113/D2Ptrs.h
@@ -55,8 +55,8 @@ FUNCPTR(D2CLIENT, GetUnitY, int __fastcall, (UnitAny* pUnit), 0x1660)
 
 FUNCPTR(D2CLIENT, ShopAction, void __fastcall, (UnitAny* pItem, UnitAny* pNpc, UnitAny* pNpc2, DWORD dwSell, DWORD dwItemCost, DWORD dwMode, DWORD _2, DWORD _3), 0x47D60)
 
-FUNCPTR(D2CLIENT, CloseNPCInteract, void __fastcall, (void), 0x48350)
-FUNCPTR(D2CLIENT, ClearScreen, void __fastcall, (void), 0x492F0) // unused but I want to look into using it
+FUNCPTR(D2CLIENT, CloseNPCInteract, void __fastcall, (void), 0x492F0)
+//FUNCPTR(D2CLIENT, ClearScreen, void __fastcall, (void), 0x492F0) // unused but I want to look into using it // wrong function--needs to be updated still
 FUNCPTR(D2CLIENT, CloseInteract, void __fastcall, (void), 0x43870)
 
 FUNCPTR(D2CLIENT, GetAutomapSize, DWORD __stdcall, (void), 0x5F080)
