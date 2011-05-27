39a1b146f324e5f3c60bb8e669f44651489bb97b
alogwe <alogwe@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2011-05-27 22:16:19 +0000

    Added libs/classes/ Added Classes:    Quest.jsclass    QuestCollection.jsclass    EquippedItemsInterface.jsclass
    Added libs/classes/examples/
    Added example:
       QuestCollection.js
    
    (Note: EquippedItemsInterface.jsclass is still super buggy.. currently causes crash if used)
    
    git-svn-id: https://subversion.assembla.com/svn/d2bs@1287 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ scripts/scripts-ntbot/libs/classes/examples/QuestCollection.js
@@ -0,0 +1,5 @@
+// Example usage
+var myQuests = new QuestCollection();
+
+// return 'Quest' object for andariel quest using QuestCollection.getQuest(actNumber, questNumber)
+var andarielQuest = myQuests.getQuest(1, 6);
