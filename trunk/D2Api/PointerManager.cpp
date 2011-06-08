892b18adc082cccc24a9643a14aa8cdbe4d21449
eBoLAtrapO <eBoLAtrapO@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2011-06-08 08:49:27 +0000

    Add PointerManager.[cpp,hpp]
    git-svn-id: https://subversion.assembla.com/svn/d2bs@1317 080cfd25-d5d9-4c59-82ea-70d02a200d8b

---
+++ trunk/D2Api/PointerManager.cpp
@@ -0,0 +1,242 @@
+#include <windows.h>
+#include <cstdio>
+
+//#include "D2Structs.h"
+#include "PointerManager.hpp"
+//#include "Patch.h"
+
+using namespace std;
+
+namespace D2Client {
+	/* D2Client Functions */
+	//PrintGameString_t PrintGameString;
+
+	/* D2Client Vars */
+	//UnitAny** PlayerUnit;
+
+	/* Assembly Routines */
+	//unsigned GetUnitFromId_I;
+}
+
+namespace D2Win {
+	/* D2Win Functions */
+
+	/* D2Win Vars */
+}
+
+namespace D2Gfx {
+	/* D2Gfx Functions */
+}
+
+namespace D2Common {
+	/* D2Common Functions */
+}
+
+namespace D2Launch {
+	/* D2Launch Vars*/
+}
+
+namespace D2Net {
+	/* D2Net Functions */
+}
+
+namespace D2Cmp {
+	/* D2Cmp Functions */
+}
+
+namespace D2Lang {\
+	/* D2Lang Functions */
+}
+
+namespace Fog {
+
+}
+
+namespace Storm {
+
+}
+
+PointerManager* PointerManager::instance = NULL;
+
+PointerManager* PointerManager::Instance()
+{
+	return instance ? instance : ((instance = new PointerManager));
+}
+
+unsigned PointerManager::GetDllOffset(int DllNo, int Offset)
+{
+	HMODULE hMod = GetModuleHandle(moduleList[DllNo]->ModuleName.c_str());
+	if(!hMod)
+		hMod = LoadLibrary(moduleList[DllNo]->ModuleName.c_str());
+
+	if(!hMod)
+		throw;
+
+	DWORD Addr = 0;
+	if(Offset < 0) {
+		Addr = (DWORD)GetProcAddress(hMod, (LPCSTR)(-Offset));
+		printf("Addr %#08X for %s Ordinal %lu\n", Addr, moduleList[DllNo]->ModuleName.c_str(), -Offset);
+	} else {
+		Addr = ((DWORD)hMod)+Offset;
+		printf("Addr %#08X for %s Offset %#08X\n", Addr, moduleList[DllNo]->ModuleName.c_str(), Offset);
+	}
+
+	if(Addr == 0)
+		throw;
+
+	return Addr;
+}
+
+void PointerManager::CleanUp() {
+	if(moduleList.size()) {
+		for(unsigned i = 0; i < moduleList.size(); i++) {
+			delete moduleList[i];
+		}
+	}
+}
+
+bool PointerManager::DefineBases() {
+	if(basesDefined)
+		return false;
+
+	if(moduleList.size())
+		throw;
+
+	int moduleCount = sizeof(dlls) / sizeof(dlls[0]); 
+	for(int i = 0; i < moduleCount; i++) {
+		ModuleInfo* modInfo = new ModuleInfo;
+		modInfo->ModuleName = dlls[i].DllName;
+		modInfo->ModuleNo = dlls[i].DllNo;
+		modInfo->BaseAddress = 0;
+		moduleList.push_back(modInfo);
+
+		unsigned BaseAddress = GetDllOffset(i, 0);
+		moduleList[i]->BaseAddress = BaseAddress;
+	}
+
+	basesDefined = true;
+
+	return true;
+}
+
+bool PointerManager::DefineOffsets()
+{
+	try
+	{
+		if(!DefineBases())
+			throw;
+	}
+	catch(const exception &ex)
+	{
+		throw(ex);
+	}
+
+	//InitPtr(D2Client::RevealAutomapRoom, D2CLIENT, 0x62580);
+
+	return true;
+}
+
+bool PatchManager::InstallPatches(void) {
+	for(int x = 0; x < ArraySize(pHooks); x++)
+	{
+		DWORD Offset = PointerManager::Instance()->GetDllOffset(pHooks[x].dwDllNo, pHooks[x].dwAddr);
+		pHooks[x].dwAddr = Offset;
+		pHooks[x].bOldCode = new BYTE[pHooks[x].dwLen];
+		if(::ReadProcessMemory(GetCurrentProcess(), (void*)Offset, pHooks[x].bOldCode, pHooks[x].dwLen, NULL) == 0) {
+			printf("ReadProcessMemory: Offset='%#08X' Err'%d'\n", Offset, GetLastError());
+			return false;
+		}
+
+		// Need better handling
+		if(pHooks[x].Type == CALL)
+			PatchCall(Offset, pHooks[x].dwFunc, pHooks[x].dwLen);
+		else if(pHooks[x].Type == JMP)
+			PatchJmp(Offset, pHooks[x].dwFunc, pHooks[x].dwLen);
+
+	}
+	return true;
+}
+
+void PatchManager::RemovePatches()
+{
+	for(int x = 0; x < ArraySize(pHooks); x++) {
+		if(!WriteBytes((void*)pHooks[x].dwAddr, pHooks[x].bOldCode, pHooks[x].dwLen)) {
+			delete[] pHooks[x].bOldCode;
+			throw;
+		}
+		delete[] pHooks[x].bOldCode;
+	}	
+}
+
+bool PatchManager::WriteBytes(void *pAddr, void *pData, DWORD dwLen)
+{
+	DWORD dwOld;
+
+	if(!VirtualProtect(pAddr, dwLen, PAGE_READWRITE, &dwOld))
+		return false;
+
+	::memcpy(pAddr, pData, dwLen);
+	return VirtualProtect(pAddr, dwLen, dwOld, &dwOld) != FALSE;
+}
+
+bool PatchManager::FillBytes(void *pAddr, BYTE bFill, DWORD dwLen)
+{
+	BYTE *bCode = new BYTE[dwLen];
+	::memset(bCode, bFill, dwLen);
+
+	if(!WriteBytes(pAddr, bCode, dwLen)) {
+		delete [] bCode;
+		return false;
+	}
+
+	delete[] bCode;
+	return true;
+}
+
+bool PatchManager::InterceptLocalCode(BYTE bInst, DWORD pAddr, DWORD pFunc, DWORD dwLen)
+{
+	BYTE *bCode = new BYTE[dwLen];
+	::memset(bCode, 0x90, dwLen);
+	DWORD dwFunc = pFunc - (pAddr + 5);
+
+	bCode[0] = bInst;
+	*(DWORD *)&bCode[1] = dwFunc;
+	if(!WriteBytes((void*)pAddr, bCode, dwLen)) {
+		delete [] bCode;
+		return false;
+	}
+
+	delete [] bCode;
+	return true;
+}
+
+void PatchManager::PatchCall(DWORD dwAddr, DWORD dwFunc, DWORD dwLen)
+{
+	if(!InterceptLocalCode(INST_CALL, dwAddr, dwFunc, dwLen))
+		throw;
+}
+
+void PatchManager::PatchJmp(DWORD dwAddr, DWORD dwFunc, DWORD dwLen)
+{
+	if(!InterceptLocalCode(INST_JMP, dwAddr, dwFunc, dwLen))
+		throw;
+}
+
+void PatchManager::PatchBytes(DWORD dwAddr, DWORD dwValue, DWORD dwLen)
+{
+	BYTE *bCode = new BYTE[dwLen];
+	::memset(bCode, (BYTE)dwValue, dwLen);
+
+	if(!WriteBytes((LPVOID)dwAddr, bCode, dwLen)) {
+		delete [] bCode;
+		throw;
+	}
+
+	delete [] bCode;
+}
+
+PatchHook *PatchManager::RetrievePatchHooks(PINT pBuffer)
+{
+	//*pBuffer = ArraySize(pHooks);
+	return &pHooks[0];
+}
---
+++ trunk/D2Api/PointerManager.hpp
@@ -0,0 +1,176 @@
+#pragma once
+
+#include <cstdio>
+#include <string>
+#include <vector>
+#include <windows.h>
+
+//#include "D2Structs.h"
+
+using namespace std;
+
+// To GO!
+#ifndef ArraySize
+#define ArraySize(x) (sizeof((x)) / sizeof((x)[0]))
+#endif
+
+enum D2DllNo {
+	D2CLIENT, 
+	D2COMMON, 
+	D2GFX, 
+	D2LANG, 
+	D2WIN, 
+	D2NET, 
+	D2GAME, 
+	D2LAUNCH, 
+	FOG, 
+	BNCLIENT, 
+	STORM, 
+	D2CMP, 
+	D2MULTI
+};
+
+struct D2DllInfo {
+	char* DllName;
+	D2DllNo DllNo;
+};
+
+//static const int MaxModNo = 13;
+const D2DllInfo dlls[] = {
+	{ "D2Client.DLL", D2CLIENT }, 
+	{ "D2Common.DLL", D2COMMON },
+	{ "D2Gfx.DLL", D2GFX },
+	{ "D2Lang.DLL", D2LANG },
+	{ "D2Win.DLL", D2WIN },
+	{ "D2Net.DLL", D2NET },
+	{ "D2Game.DLL", D2GAME },
+	{ "D2Launch.DLL", D2LAUNCH },
+	{ "Fog.DLL", FOG },
+	{ "BNClient.DLL", BNCLIENT },
+	{ "Storm.DLL", STORM },
+	{ "D2Cmp.DLL", D2CMP },
+	{ "D2Multi.DLL", D2MULTI}
+};
+
+struct ModuleInfo {
+	string ModuleName;
+	int ModuleNo;
+	DWORD BaseAddress;
+};
+
+struct Offset {
+	ModuleInfo* Module;
+	DWORD BaseOffset;
+};
+
+typedef vector<Offset*> Offsets;
+typedef vector<ModuleInfo*> Modules;
+
+namespace D2Client {
+	typedef void (__stdcall *PrintGameString_t)(wchar_t *wMessage, int nColor);
+
+	extern PrintGameString_t PrintGameString;
+}
+
+namespace D2Win {
+}
+
+namespace D2Gfx {
+}
+
+namespace D2Common {
+}
+
+namespace D2Launch {
+}
+
+namespace D2Net {
+}
+
+namespace D2Cmp {
+}
+
+namespace D2Lang {
+}
+
+namespace Fog {
+}
+
+namespace Storm {
+}
+
+class PointerManager {
+private:
+	static PointerManager* instance;
+
+	Offsets offsetList;
+	Modules moduleList;
+	bool basesDefined;
+	bool offsetsDefined;
+
+	//ModuleInfo* GetModuleInfo(int num);
+
+	template<typename S>
+	S & InitPtr(S & Symbol, int Dllno, int Offset) {
+		Symbol = reinterpret_cast<S>(GetDllOffset(Dllno, Offset));
+		return Symbol;
+	}
+	template<>
+	unsigned int & InitPtr<unsigned int>(unsigned int & Symbol, int Dllno, int Offset) {
+		Symbol = GetDllOffset(Dllno, Offset);
+		return Symbol;
+	}
+
+	PointerManager() : basesDefined(false), offsetsDefined(false) {}
+	~PointerManager() { CleanUp(); }
+	PointerManager(const PointerManager &);
+	PointerManager & operator= (PointerManager const &);
+
+public:
+	static PointerManager * Instance();
+	unsigned GetDllOffset(int DllNo, int Offset);
+
+	bool DefineOffsets();
+	bool DefineBases();
+
+	void CleanUp();
+};
+
+#define INST_INT3	0xCC
+#define INST_CALL	0xE8
+#define INST_NOP	0x90
+#define INST_JMP	0xE9
+#define INST_RET	0xC3
+
+enum PatchType {
+	CALL,
+	JMP
+};
+
+typedef struct PatchHook_t
+{
+	PatchType Type;
+	DWORD dwDllNo;
+	DWORD dwAddr;
+	DWORD dwFunc;
+	DWORD dwLen;
+	BYTE *bOldCode;
+} PatchHook;
+
+class PatchManager {
+private:
+	PatchHook *RetrievePatchHooks(PINT pBuffer);
+	void PatchBytes(DWORD dwAddr, DWORD dwValue, DWORD dwLen);
+	void PatchJmp(DWORD dwAddr, DWORD dwFunc, DWORD dwLen);
+	void PatchCall(DWORD dwAddr, DWORD dwFunc, DWORD dwLen);
+	bool InterceptLocalCode(BYTE bInst, DWORD pAddr, DWORD pFunc, DWORD dwLen);
+	bool FillBytes(void *pAddr, BYTE bFill, DWORD dwLen);
+	bool WriteBytes(void *pAddr, void *pData, DWORD dwLen);
+
+public:
+	PatchManager() {}
+	~PatchManager() {}
+
+	void RemovePatches();
+	bool InstallPatches();
+};
