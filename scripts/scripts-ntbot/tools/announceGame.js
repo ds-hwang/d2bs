b16b238a84e831bc2d9487e40954e51b21f5135e
alogwe <alogwe@080cfd25-d5d9-4c59-82ea-70d02a200d8b>
2011-07-09 16:54:08 +0000

    Added option to announce in multiple irc channels Fixed the mIRC script timer to allow one announcement per char every 15 seconds instead of one announcement every 15 seconds
    git-svn-id: https://subversion.assembla.com/svn/d2bs@1342 080cfd25-d5d9-4c59-82ea-70d02a200d8b

--- scripts/scripts-ntbot/tools/announceGame.js
+++ scripts/scripts-ntbot/tools/announceGame.js
@@ -1,9 +1,22 @@
-//	Save this script in your /tools/ folder 
-
-//	Load this script by adding the following line to your 
-//	runner's char config file (anywhere on a line by itself: (take out the comment though)
-//	
-//	load('tools/announceGame.js');
+/**
+*	@title			announceGame.js
+*
+*	@desc			Formats and sends a message to an active mIRC client via sendDDE()
+*					Accompanying mIRC script will format the announcement and display in chosen channels
+*
+*	@requires		Installation of mIRC script which is commented at the bottom of this script
+*
+*	@installation	Save this script to your /tools/ dir
+*					Load this script by adding the following line to your runner's char config file (or anywhere in default.dbj)
+*
+*					load('tools/announceGame.js');
+*
+*					To install the mIRC script, copy the commented section at the bottom of this file between the multiline 
+*					comment brackets and paste that into a new file under your Remote scripts (ALT + R) section of the program
+*					
+*					Set your announce channels up under the line			;; ADD / CHANGE ANNOUNCE CHANNELS HERE ;;
+*					You can format your announce message under the line		;; FORMAT / CHANGE ANNOUNCE MESSAGE HERE ;;
+*/
 
 function main(){
 	announceGame();
@@ -47,37 +60,77 @@ function announceGame(){
 /*	mIRC Script for Remote Scripts section ;; Add this Alias to your Remote scripts in mIRC (Alt+R) and change the %announceChan var.
 
 
+
+
+
 Alias announceGame {
 
-  ;; Format: /announceGame realm core gameType charName charLvl charClass gameDiff gameName gamePassword
 
-  ;; Workaround for NTBot spamming the sendDDE() func from the script when in char config file..
-  ;; (only allows mIRC to announceGame once every 15 seconds)
-  
-  var %announceChan = "#IRCCHANNELHERE"
 
-  if ($var(gameAnnounceActive,0) > 0) { return }
-  else { set -u15 %gameAnnounceActive true }
+  ;; ADD / CHANGE ANNOUNCE CHANNELS HERE ;;
+  addAnnounceChan #IRCCHANNELHERE
+  addAnnounceChan #SOME_OTHER_CHANNEL
+  addAnnounceChan #ADD_MORE_LIKE_THIS_ETC
 
-  if ($me ison %announceChan) {
-    var %gameRealm = $1
-    var %gameCore = $2
-    var %gameType = $3
-    var %charName = $4
-    var %charLvl = $5
-    var %charClass = $6
-    var %gameDiff = $7
-    var %gameName = $8
-    var %gamePassword = $9
 
-    if ( baal isin $lower($gameName)) { var %runType = new BAAL }
-    else if ( chaos isin $lower($gameName)) { var %runType = new CHAOS }
-    else { var %runType = new }
 
-    msg %announceChan (10 $+ %gameRealm %gameCore %gameType $+ 1) 7 $+ %charName $+ , my level %charLvl %charClass has started a %runType game:4  %gameName 1//4 %gamePassword
+  var %charName = $4
+
+  ;; Workaround for NTBot spamming sendDDE() from within char config (Only allows 1 announcement per charName every 15 seconds)
+
+  if ($var( [ [ %charName ] $+ _announced ] , 0) > 0) { return }
+  else { set -u15 [ % $+ [ %charName ] $+ _announced ] true }
+
+  var %gameRealm = $1
+  var %gameCore = $2
+  var %gameType = $3
+  var %charLvl = $5
+  var %charClass = $6
+  var %gameDiff = $7
+  var %gameName = $8
+  var %gamePassword = $9
+
+  if ( $count($lower(%gameName), baal) > 0 ) { var %runType = new BAAL }
+  else if ( $count($lower(%gameName), chaos) > 0 ) { var %runType = new CHAOS }
+  else { var %runType = new }
+
+
+
+  ;; FORMAT / CHANGE ANNOUNCE MESSAGE HERE ;;
+  var %announceMsg (10 $+ %gameRealm %gameCore %gameType $+ 1) 7 $+ %charName $+ , my level %charLvl %charClass has started a %runType game:4  %gameName 1//4 %gamePassword
+
+
+
+  ; Get total number of announceChans
+  var %numAnnounceChans = $numtok(%announceChans, 32)
+
+  ; Iterate across announceChans and send announce message to each
+  var %count = 1
+
+  while (%count <= %numAnnounceChans) {
+    var %currentChan = $gettok(%announceChans, %count, 32)
+
+    if ($me ison %currentChan) {
+      msg %currentChan %announceMsg
+    }
+
+    inc %count
   }
 
 }
+
+
+Alias addAnnounceChan {
+  set %announceChans $addtok(%announceChans, $1, 32)
+}
+
+
+Alias remAnnounceChan {
+  set %announceChans $remtok(%announceChans, $1, 32)
+}
+
+
+
 
 
 */
